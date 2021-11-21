644dc0cfffe044be875ff27e4c902b237aabe90a
Itsqy <syatriarifqi@gmail.com>
2021-11-21 18:36:58 +0700

    ghsgdhs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -77,7 +77,7 @@
                     <td scope="row">{{ $response['result']['id'] }}</td>
                     <td>{{ $response['result']['name'] }}</td>
                     <td>{{ $response['result']['email'] }}</td>
-                    {{-- <td>{{ $response['result']['telp'] }}</td> --}}
+                    <td>{{ $response['result']['telp'] }}</td>
                     <td> {{ $response['result']['address'] }}</td>
                     <td>{{ $response['result']['photo'] }}</td>
                     <td>{{ $response['result']['created_at'] }}</td>
